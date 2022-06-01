9e64ab36daa3183924b83b8225f86c5448ab543d
Matthias Kluth <m.kluth@ipi-gmbh.com>
2022-06-01 12:33:35 +0200

    cleanup

--- src/features/Login/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Login = () => {
-  return (
-    <div id='login'>Login</div>
-  )
-}
-
-export default Login
--- src/setupTests.js
+++
@@ -1,8 +0,0 @@
-/* const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-
-global.document = JSDOM('<!doctype html><html><body><div id="root"></div></body></html>');
-global.window = document.defaultView;
-global.navigator = window.navigator; */
-
-import '@testing-library/jest-dom'
