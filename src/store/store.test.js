b0582cf9ca7a6e48bf4e1f0a3db12af103fe79fa
Matthias Kluth <m.kluth@ipi-gmbh.com>
2022-06-06 22:40:58 +0200

    feat(tests): Leaderboard and store Both parts with a coverage of 100%

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,45 +1,141 @@
-import { applyMiddleware, legacy_createStore as createStore } from 'redux'
 import rootReducer from './reducers'
-import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
-import { render } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
 import * as API from '../_DATA'
-import React from 'react'
 
-import App from '../App'
 
 API._getQuestions = jest.fn()
 API._getUsers = jest.fn()
 
-// mock useEffect
-const effectSpy = jest.spyOn(React, 'useEffect')
 
-const renderWithProvider = (ui) => {
-    const store = createStore(rootReducer, applyMiddleware(thunk))
-    return render(
-        <Provider store={store}>
-            <BrowserRouter>
-                {ui}
-            </BrowserRouter>
-        </Provider>
-    )
-}
+describe('Redux Reducers', () => {
+    describe('Questions Reducer', () => {
+        it('should return the initial state', () => {
+            expect(rootReducer({}, {})).toEqual({
+                questions: {},
+                users: {},
+                auth: {}
+            })
+        })
+        it('should handle GET_QUESTIONS', () => {
+            expect(
+                rootReducer({}, {
+                    type: 'GET_QUESTIONS'
+                })
+            ).toEqual({
+                questions: {},
+                users: {},
+                auth: {},
+            })
+        })
+        it('should handle SET_QUESTIONS', () => {
+            expect(
+                rootReducer({}, {
+                    type: 'SET_QUESTIONS',
+                    payload: [{
+                        questions: [{
+                            id: 1,
+                            optionOne: {
+                                votes: [],
+                                text: 'test'
+                            },
+                            optionTwo: {
+                                votes: [],
+                                text: 'test'
+                            },
+                        }]
+                    }]
+                })
+            ).toEqual({
+                questions: [{
+                    questions: [{
+                        id: 1,
+                        optionOne: {
+                            votes: [],
+                            text: 'test'
+                        },
+                        optionTwo: {
+                            votes: [],
+                            text: 'test'
+                        },
+                    }],
+                }
+                ],
+                users: {},
+                auth: {},
+            })
+        })
+        it('should handle SET_OLD_QUESTIONS', () => {
+            expect(
+                rootReducer({}, {
+                    type: 'SET_OLD_QUESTIONS',
+                    payload: [{
 
-describe('Redux State', () => {
-    it('should fire the useEffect hook', () => {
-        renderWithProvider(<App />)
-        expect(effectSpy).toHaveBeenCalled()
-    })
+                        id: 1,
+                        optionOne: {
+                            votes: [],
+                            text: 'test'
+                        },
+                        optionTwo: {
+                            votes: [],
+                            text: 'test'
+                        },
+                    }]
+                })
+            ).toEqual({
+                questions: {
+                    old: [
+                        {
+                            id: 1,
+                            optionOne: {
+                                votes: [],
+                                text: 'test'
+                            },
+                            optionTwo: {
+                                votes: [],
+                                text: 'test'
+                            },
+                        }
+                    ]
+                },
+                users: {},
+                auth: {},
+            })
+        })
+        it('should handle SET_NEW_QUESTIONS', () => {
+            expect(
+                rootReducer({}, {
+                    type: 'SET_NEW_QUESTIONS',
+                    payload: [{
 
-    // make sure the correct methods are called
-    it('should call the API to get the questions', () => {
-        renderWithProvider(<App />)
-        expect(API._getQuestions).toHaveBeenCalled()
-    })
-
-    it('should call the API to get the users', () => {
-        renderWithProvider(<App />)
-        expect(API._getUsers).toHaveBeenCalled()
+                        id: 1,
+                        optionOne: {
+                            votes: [],
+                            text: 'test'
+                        },
+                        optionTwo: {
+                            votes: [],
+                            text: 'test'
+                        },
+                    }]
+                })
+            ).toEqual({
+                questions: {
+                    new: [
+                        {
+                            id: 1,
+                            optionOne: {
+                                votes: [],
+                                text: 'test'
+                            },
+                            optionTwo: {
+                                votes: [],
+                                text: 'test'
+                            },
+                        }
+                    ]
+                },
+                users: {},
+                auth: {},
+            })
+        })
     })
 })
