d52f80d9754d4355e65984c37d717d9c05939461
Matthias Kluth <kluthmatthias@gmail.com>
2022-06-07 08:57:15 +0200

    Development (#23)
    * clean react setup
    
    * feat(react-router-dom): added basic router func
    
    * feat(redux): Login and Question sorting
    
    * feat(questions): sorting and answering
    
    * feat(questions): saving questions
    
    * feat(tests): Dashboard and Header
    Both components have a coverage of 100%
    
    * feat(tests): Leaderboard and store
    Both parts with a coverage of 100%
    
    * feat(tests): Addition and deletion
    Removed obsolete test-files, added some more tests
    
    * fix(udacity): Added some features for udacity (#18)
    
    Added a README aswell. Have fun!
    
    Co-authored-by: Matthias Kluth <m.kluth@ipi-gmbh.com>
    
    * Feature/tesing (#20)
    
    * fix(udacity): Added some features for udacity
    Added a README aswell. Have fun!
    
    * fix(devtools): removed the devtools part
    
    Co-authored-by: Matthias Kluth <m.kluth@ipi-gmbh.com>
    
    * Feature/tesing (#22)
    
    * fix(udacity): Added some features for udacity
    Added a README aswell. Have fun!
    
    * fix(devtools): removed the devtools part
    
    * fix(login): dropdown
    The test users are in a dropdown now for more comfortable testing
    
    * fix(login): login has dropdown now
    
    Co-authored-by: Matthias Kluth <m.kluth@ipi-gmbh.com>
    
    Co-authored-by: Matthias Kluth <m.kluth@ipi-gmbh.com>

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { login } from '../../store/actions/authActions'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import {
+    getUsers
+} from '../../store/actions/userActions'
 
 const Login = () => {
+    const users = useSelector(state => state.users)
     const dispatch = useDispatch()
     const handleLogin = (event) => {
         event.preventDefault()
-        const { username, password } = event.target
-        login(dispatch, { id: username.value, password: password.value })
+        const { username } = event.target
+        // set password to the users password in the store
+        const password = users.find(user => user.id === username.value).password
+        login(dispatch, { id: username.value, password: password })
     }
+    useEffect(() => {
+        getUsers(dispatch)
+    }, [dispatch])
   return (
       <form data-testid='loginForm' onSubmit={handleLogin}>
           <div className='login-form'>
@@ -18,11 +27,11 @@ const Login = () => {
               <div className='login-form-body'>
                   <div className='login-form-body-input'>
                       <label htmlFor="username">Username</label>
-                      <input type="text" name="username" id="username" data-testid='username' />
-                  </div>
-                  <div className='login-form-body-input'>
-                      <label htmlFor="password">Password</label>
-                      <input type="password" name="password" id="password" data-testid='password' />
+                      {users &&
+                      <select name="username" id="username">
+                          {Object.values(users).map(user => <option key={user.id} value={user.id}>{user.name}</option>)}
+                        </select>
+                      }
                   </div>
                   <div className='login-form-body-button'>
                       <button type="submit" name='Login' data-testid='login'>Login</button>
